b4f510e6054c1114eba62295500a282320a9322d
ezra sandzer-bell and taylor loftis-kim <ezra@>
2016-10-20 15:34:05 -0700

    create a fully functional calculator

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number3, number4) {
+  return number3 - number4;
+};
+
+var multiply = function(number5, number6) {
+  return number5 * number6;
+};
+
+var divide = function(number7, number8) {
+  return number7 / number8;
+};
+
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
+});
